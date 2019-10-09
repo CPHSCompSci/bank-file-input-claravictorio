9259c438a48295c2ac1118da151af15f7f707aaf
clara <clara@DESKTOP-C33H0K7>
2019-10-09 16:08:29 -0500

    created menu options

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,19 +6,47 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		example1();
-	}
-	
-	public static void example1()
-	{
+		
+		Bank bank = new Bank("Knab Bank");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("1. Deposit");
 		System.out.println("2. Withdraw");
 		System.out.println("3. Check balance");
 		System.out.println("4. Create account");
-		System.out.println("5. Transfer funds");
-		System.out.println("6. Close Account");
+		System.out.println("5. Close Account");
+		System.out.println("6. Transfer funds");
 		System.out.println("Select an option: ");
 		int selection = scan.nextInt();
+		
+		if (selection == 1)
+		{
+			
+		}
+		else if (selection == 2)
+		{
+			
+		}
+		else if (selection == 3)
+		{
+			
+		}
+		else if (selection == 4)
+		{
+			
+		}
+		else if (selection == 5)
+		{
+			
+		}
+		else if (selection == 6)
+		{
+			
+		}
+		
+	}
+	
+	public static void example1()
+	{
 		
 		Bank bank = new Bank("Bank of CPHS");
 		int ewbankAccountNo = bank.createAccount("Mr. Ewbank");
