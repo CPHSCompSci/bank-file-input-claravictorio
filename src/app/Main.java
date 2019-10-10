5de584568494d4e6b6da82ad2c20d93b3e8e9d95
victocla <victocla@014-1617-S28JJ.students.cisd.conroeisd.loc>
2019-10-10 12:48:09 -0500

    front end/user input

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-		example1();
+		// example1();
 		
-		Bank bank = new Bank("Knab Bank");
+		Bank bank = new Bank("The Bank");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("1. Deposit");
 		System.out.println("2. Withdraw");
@@ -17,34 +17,62 @@ public class Main {
 		System.out.println("6. Transfer funds");
 		System.out.println("Select an option: ");
 		int selection = scan.nextInt();
+		scan.nextLine();
+		int acctno;
+		int amt;
 		
 		if (selection == 1)
 		{
-			
+			System.out.println("Enter your account number: ");
+			acctno = scan.nextInt();
+			System.out.println("How much would you like to deposit?");
+			amt = scan.nextInt();
+			bank.deposit(acctno, amt);
 		}
 		else if (selection == 2)
 		{
-			
+			System.out.println("Enter your account number: ");
+			acctno = scan.nextInt();
+			System.out.println("How much would you like to withdraw?");
+			amt = scan.nextInt();
+			bank.withdraw(acctno, amt);	
 		}
 		else if (selection == 3)
 		{
-			
+			System.out.println("Enter your account number: ");
+			acctno = scan.nextInt();
+			bank.checkBalance(acctno);
 		}
 		else if (selection == 4)
 		{
-			
+			System.out.println("Enter your name: ");
+			String name = scan.nextLine();
+			bank.createAccount(name);
 		}
 		else if (selection == 5)
 		{
-			
+			System.out.println("Enter your account number: ");
+			acctno = scan.nextInt();
+			bank.closeAccount(acctno);
 		}
 		else if (selection == 6)
 		{
+			System.out.println("Enter your account number: ");
+			int fromAcct = scan.nextInt();
+			System.out.println("Enter the number of the account you would like to transfer money to: ");
+			int toAcct = scan.nextInt();
+			System.out.println("How much money would you like to transfer? ");
+			amt = scan.nextInt();
+			bank.withdraw(fromAcct, amt);
+			bank.deposit(toAcct, amt);
 			
 		}
 		
+		
 	}
 	
+	
+	/*
 	public static void example1()
 	{
 		
@@ -65,4 +93,5 @@ public class Main {
 		bank.checkBalance(howardAccountNo);
 		
 	}
+	*/
 }
